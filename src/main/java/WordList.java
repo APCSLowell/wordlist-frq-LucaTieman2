7ea9d8487c56dd3f83f91c8a89f977a4a73c7af7
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-04-03 09:48:06 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,7 +12,7 @@ public class WordList
   {
     int amount = 0; 
     for (int i = 0; i < myList.size(); i++) {
-      if (myList.get(i).length() == len) {
+      if (myList.get(i).size() == len) {
         amount++;
       }
     }
@@ -22,7 +22,7 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     for (int i = 0; i < myList.size(); i++) {
-      if (myList.get(i).length() == len) {
+      if (myList.get(i).size() == len) {
         myList.remove(i);
         i--;
       }
